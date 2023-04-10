923325aeb7dbc1d6f5a470350f78de46e0fb236f
rehmanalira <mabdurrehmanra@gmail.com>
2023-04-10 23:34:55 +0500

    Third Commit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
